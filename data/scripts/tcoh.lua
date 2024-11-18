278162c537ef5844cf6bc136d531027f0f86db33
Nikita <n_ksh1v@outlook.com>
2024-11-18 06:19:52 +0300

    finished first part of storyline in Vahat
    heroes are going to Zarmek

--- data/scripts/tcoh.lua
+++ data/scripts/tcoh.lua
@@ -119,6 +119,7 @@ function CreateMansurAndParents()
 	local Mansur = getObj("MansurCar")
 	if Mansur then
 		Mansur:SetRotation(Quaternion(0.002, -0.932, -0.003, -0.362))
+		Mansur:SetSkin(1)
 	end
 
 	local Parents = getObj("ParentsCar")
@@ -153,4 +154,116 @@ function CinematicMessageMSWPCam01()
 	AddCinematicMessage( 138, 0.2 )
 	AddCinematicMessage( 139, 0.2 )
 	AddCinematicMessage( 140, 0.2 )
+end
+
+function CreateBuharaAndAivanJrAttackers()
+	local attm = CreateTeam("BuharaAttack", 1025, CVector(1706.407, 231.845, 6608.407),{"UralShot1","UralShot1","Ural01","Cruiser01"}, CVector(1587.880, 229.640, 6611.723), nil, Quaternion(-0.052, 0.996, 0.024, -0.061))
+		if attm then
+			AddVehicleGunsWithRandomAffix( attm:GetVehicle(0), "kpvt01", 3, 10)
+			attm:GetVehicle(1):AddItemsToRepository("add_stability_and_speed", 1)
+			AddVehicleGunsWithRandomAffix( attm:GetVehicle(2), "hunterSideGun",  3, 8+random(2))
+			AddVehicleGunsWithRandomAffix( attm:GetVehicle(3), "vulcan01", 3, 10)
+		end
+
+	local veh0 = getObj("BuharaAttack_vehicle_0")
+		if veh0 then
+			veh0:SetGamePositionOnGround(CVector(getPos("BuharaAttackersSpawn0_loc")))
+			veh0:SetRotation(Quaternion(-0.052, 0.996, 0.024, -0.061))
+			veh0:SetRandomSkin()
+		end
+
+	local veh1 = getObj("BuharaAttack_vehicle_1")
+		if veh1 then
+			veh1:SetGamePositionOnGround(CVector(getPos("BuharaAttackersSpawn1_loc")))
+			veh1:SetRotation(Quaternion(-0.052, 0.996, 0.024, -0.061))
+			veh1:SetRandomSkin()
+		end
+
+	local veh2 = getObj("BuharaAttack_vehicle_2")
+		if veh2 then
+			veh2:SetGamePositionOnGround(CVector(getPos("BuharaAttackersSpawn2_loc")))
+			veh2:SetRotation(Quaternion(-0.052, 0.996, 0.024, -0.061))
+			veh2:SetRandomSkin()
+		end
+
+	local veh3 = getObj("BuharaAttack_vehicle_3")
+		if veh3 then
+			veh3:SetGamePositionOnGround(CVector(getPos("BuharaAttackersSpawn3_loc")))
+			veh3:SetRotation(Quaternion(-0.052, 0.996, 0.024, -0.061))
+			veh3:SetRandomSkin()
+		end
+end
+
+function CreateBuharaAndAivanJr()
+	CreateTeam("Buharers",1026, CVector(getPos("BuhSpawn_loc")),{"DemoUral1","DemoMolokovoz1"})
+	CreateTeam("AivanJr", 1008, CVector(getPos("BuhSpawn_loc")),{"BelazShot1"})
+
+	local buhara0 = getObj("Buharers_vehicle_0")
+	if buhara0 then
+		buhara0:SetGamePositionOnGround(CVector(getPos("BuhDefSpawn0_loc")))
+		buhara0:SetRotation(Quaternion(-0.022, 0.083, -0.023, 0.996))
+		buhara0:SetSkin(0)
+	end
+
+	local buhara1 = getObj("Buharers_vehicle_1")
+	if buhara1 then
+		buhara1:SetGamePositionOnGround(CVector(getPos("BuhDefSpawn1_loc")))
+		buhara1:SetRotation(Quaternion(-0.022, 0.083, -0.023, 0.996))
+		buhara1:SetSkin(0)
+	end
+
+	local aivanjr = getObj("AivanJr_vehicle_0")
+	if aivanjr then
+		aivanjr:SetGamePositionOnGround(CVector(getPos("AivanJrDefSpawn_loc")))
+		aivanjr:SetRotation(Quaternion(-0.022, 0.083, -0.023, 0.996))
+		aivanjr:SetSkin(3)
+	end
+end
+
+function SaveBuharaQuestTest()
+	AddPlayerVehicle("UralDadHamza")
+
+	local PVeh = GetPlayerVehicle()
+	if PVeh then
+		PVeh:SetGamePositionOnGround(CVector(getPos("FirstMeetAivanJr_SaveThem_loc")))
+		PVeh:SetRotation(Quaternion(0.002, 0.101, 0.009, 0.995))
+		PVeh:SetSkin(7)
+	end
+
+	TActivate("ConnectMansur_GoToBuhara_Again")
+	TActivate("FirstMeetAivanJr_SaveThem_cs")
+end
+
+function BuharaEvacutedCreateCars()
+	local PlVeh = GetPlayerVehicle()
+	if PlVeh then
+		PlVeh:SetGamePositionOnGround(CVector(getPos("BuharaEvacuated_cs_Player_loc")))
+		PlVeh:SetRotation(Quaternion(0.017, -0.297, 0.018, 0.954))
+	end
+
+	local vehB1 = CreateVehicleEx("DemoUral1","Buhara_0",CVector(getPos("BuhSpawn_loc")), 1026)
+	if vehB1 then
+		vehB1:SetGamePositionOnGround(CVector(getPos("BuharaEvacuated_cs_Buhara0_loc")))
+		vehB1:SetRotation(Quaternion(0.020, 0.672, -0.008, 0.740))
+		vehB1:SetSkin(0)
+	end
+
+	local veh2 = CreateVehicleEx("DemoMolokovoz1","Buhara_1",CVector(getPos("BuhSpawn_loc"))+CVector(7, 0, 7), 1026)
+	if veh2 then
+		veh2:SetGamePositionOnGround(CVector(getPos("BuharaEvacuated_cs_Buhara1_loc")))
+		veh2:SetRotation(Quaternion(0.010, 0.880, 0.003, 0.475))
+		veh2:SetSkin(0)
+	end
+
+	local ManCarCreate = CreateVehicleEx("UralMansur", "MansurCarCS", CVector(getPos("BuharaEvacuated_cs_Mansur_loc")), 1090)
+	if ManCarCreate then
+		ManCarCreate:SetRotation(Quaternion(0.023, -0.814, -0.019, 0.580))
+		ManCarCreate:SetSkin(1)
+	end
+
+	local AivenCreate = CreateVehicleEx("BelazShot1", "MansurCarCS", CVector(getPos("BuharaEvacuated_cs_AivenJr_loc")), 1008)
+	if AivenCreate then
+		AivenCreate:SetRotation(Quaternion(-0.006, 0.922, 0.008, -0.387))
+		AivenCreate:SetSkin(3)
+	end
 end
